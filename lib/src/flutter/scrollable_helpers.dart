76fd87bf929046c3c453b6bde638952c34ad8cca
Saif <saif1988@gmail.com>
2024-11-27 11:22:19 +0100

    Comment assertion that is unclear

--- lib/src/flutter/scrollable_helpers.dart
+++ lib/src/flutter/scrollable_helpers.dart
@@ -232,12 +232,12 @@ class EdgeDraggingAutoScroller {
         scrollRenderBox.size.height,
       ),
     );
-    assert(
-      globalRect.size.width >= _dragTargetRelatedToScrollOrigin.size.width &&
-          globalRect.size.height >=
-              _dragTargetRelatedToScrollOrigin.size.height,
-      'Drag target size is larger than scrollable size, which may cause bouncing',
-    );
+    // assert(
+    //   globalRect.size.width >= _dragTargetRelatedToScrollOrigin.size.width &&
+    //       globalRect.size.height >=
+    //           _dragTargetRelatedToScrollOrigin.size.height,
+    //   'Drag target size is larger than scrollable size, which may cause bouncing',
+    // );
     _scrolling = true;
     double? newOffset;
     const double overDragMax = 20.0;
